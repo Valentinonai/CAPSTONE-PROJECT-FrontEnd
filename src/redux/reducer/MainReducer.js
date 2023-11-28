f39a0545d0b75af4363eb2fcbadfed7a5ee53ead
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-28 13:44:47 +0100

    setup progetto

---
+++ src/redux/reducer/MainReducer.js
@@ -0,0 +1,7 @@
+const initialState = {};
+
+const MainReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default MainReducer;
---
+++ src/redux/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import MainReducer from "../reducer/MainReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_LOCALSTORAGEKEY,
+    }),
+  ],
+  // whitelist:[];
+};
+
+const rootReducer = combineReducers({
+  mainReducer: MainReducer,
+});
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducers,
+  middleware: getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }),
+});
+
+export const persiStore = persistStore(store);
